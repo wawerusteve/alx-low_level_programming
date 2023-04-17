865f2735ba207790b9843b90cf76c8ba6b135254
wawerusteve <getafeleo@gmail.com>
2023-04-17 11:52:38 -0700

    0x0D. C - Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- *main - prints the name of file it was compiled from
+ * main - prints the name of the file
  *
- *Return:(0) - success
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
